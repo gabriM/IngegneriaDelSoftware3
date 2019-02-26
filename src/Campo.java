5aec4383cb4e3a6ccf94760a0cea67def9e2e17c
gabriM <g.manenti013@studenti.unibs.it>
2019-02-26 01:41:18 +0100

    Inserita parte javadoc

--- src/Campo.java
+++ src/Campo.java
@@ -4,38 +4,152 @@ import java.text.SimpleDateFormat;
 
 import MyLib.Utility;
 
+/**
+ * Classe che definisce un <i>Campo</i>.
+ *
+ * Un campo contiene i dati riguardanti un caratteristca di un determinato evento:
+ * nome;
+ * descrizione;
+ * obbligatorietà del campo;
+ * valore del campo.
+ *
+ * @author Gabriele Manenti
+ *
+ * @version 3.0 1 Febbraio 2019
+ *
+ */
+
 public class Campo implements Serializable{
 	
-	//Attributi
+	/*Attributi*/
 	private String nome;
 	private String descrizione;
 	private Boolean obbligatorio;
 	private Valore valore;
 	
 	
-	//Costruttori
+	/*Costruttori*/
+	/**
+	 * Un campo e' costituito da nome, descrizione, obbligatorietà e valore riguardante una caratteristica di un evento:
+	 *
+	 * @param _nome nome del campo
+	 * @param _descrizione descrizione del campo
+	 * @param _obbligatorio obbligatorietà del campo
+	 * @param _valore tipologia del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+
 	public Campo(String _nome, String _descrizione, Boolean _obbligatorio, int _valore){
 		nome =_nome;
 		descrizione= _descrizione;
 		obbligatorio = _obbligatorio;
 		valore= new Valore(_valore);
 	}
-	
-	//Metodi
-	
-	// Metodo che restituisce una stringa che descrive il campo
-	public String visualizzaCampo(){
-		String s= new String("Nome: "+ nome +"\n" +"Descrizione: " + descrizione+ "\n"+ "Obbligatoria: " + obbligatorio +"\n");
-		return s;
+
+	/*Getters*/
+	/**
+	 * Ritorna il nome del Campo
+	 * @return nome del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public String getNome() {
+		return nome;
 	}
-	
-	// Metodo che inserisce assegna un valore al campo
+	/**
+	 * Ritorna la descrizione del Campo
+	 * @return descrizione del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public String getDescrizione() {
+		return descrizione;
+	}
+	/**
+	 * Ritorna l'obbligatorietà del Campo
+	 * @return se un campo è obbligatorio o meno
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public Boolean getObbligatorio() {
+		return obbligatorio;
+	}
+	/**
+	 * Ritorna il valore del Campo
+	 * @return valore del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public Valore getValore() {
+		return valore;
+	}
+
+	/*Setters*/
+	/**
+	 * Metodo che imposta il nome del Campo.
+	 *
+	 * @param nome il nome del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * Metodo che imposta la descrizione del Campo.
+	 *
+	 * @param descrizione la descrizione del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	/**
+	 * Metodo che imposta l'obbligatorietà di un Campo.
+	 *
+	 * @param obbligatorio l'obbligatorietà del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public void setObbligatorio(Boolean obbligatorio) {
+		this.obbligatorio = obbligatorio;
+	}
+	/**
+	 * Metodo che imposta il valore di un Campo.
+	 *
+	 * @param valore il valore del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public void setValore(Valore valore) {
+		this.valore = valore;
+	}
+	/*Metodi*/
+	/**
+	 * Ritorna una stringa che descrive il campo
+	 *
+	 * @return info le informazioni relative ad un campo
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public String visualizzaCampo(){
+		String info= new String("Nome: "+ nome +"\n" +"Descrizione: " + descrizione+ "\n"+ "Obbligatoria: " + obbligatorio +"\n");
+		return info;
+	}
+
+	/**
+	 * Assegna un valore al campo inserito
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public void inserisciValore()throws Exception{
 		
 	if (!valore.getInserito()){
 		
 		if(!valore.getInserito()){
-			int inserimento= Utility.leggiIntero(0,1, "Vuoi inserire "+ nome+ "? Digita 1 per SI e 0 pre NO");
+			int inserimento= Utility.leggiIntero(0,1, "Vuoi inserire "+ nome+ "? Digita 1 per SI e 0 per NO");
 			if(inserimento==0){
 			
 			}
@@ -66,38 +180,7 @@ public class Campo implements Serializable{
 	
 	
 	
-	// Getters and Setters generati automaticamente
-	public String getNome() {
-		return nome;
-	}
 
-	public String getDescrizione() {
-		return descrizione;
-	}
-
-	public Boolean getObbligatorio() {
-		return obbligatorio;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-
-	public void setObbligatorio(Boolean obbligatorio) {
-		this.obbligatorio = obbligatorio;
-	}
-
-	public Valore getValore() {
-		return valore;
-	}
-
-	public void setValore(Valore valore) {
-		this.valore = valore;
-	}
 	
 	
 	
--- src/Categoria.java
+++ src/Categoria.java
@@ -4,39 +4,73 @@ import java.util.ArrayList;
 
 import java.util.ArrayList;
 
-
+/**
+ * Classe che definisce una <i>Categoria</i>.
+ *
+ * Una categoria contiene i dati riguardanti un determinato evento:
+ * nome;
+ * descrizione;
+ * titolo;
+ * numero partecipanti;
+ * numero di tolleranza partecipanti
+ * data termine iscrizione;
+ * luogo;
+ * data;
+ * ora;
+ * durata;
+ * quota individuale di partecipazione;
+ * compreso nella quota;
+ * data finale;
+ * data ritiro iscrizione;
+ * ora finale;
+ * note;
+ * elenco dei campi.
+ *
+ *
+ * @author Gabriele Manenti
+ *
+ * @version 3.0 1 Febbraio 2019
+ *
+ */
 public class Categoria implements Serializable{
 	
-	//Costanti
+	/*Costanti*/
 	public static final int STRING = 0;
 	public static final int INT = 1;
 	public static final int DATE = 2;
 	public static final int ORA = 3;
 	
-	
-	
-	
-	
+	/*Attributi*/
 	private String nome;
 	private String descrizione;
-	private Campo titolo=new Campo("Titolo","Consiste in un nome di fantasia attribuito all�evento",false,STRING);
-	private Campo nPartecipanti=new Campo("Numero di partecipanti"," Stabilisce il numero di persone da coinvolgere nell�evento",true,INT);
+	private Campo titolo=new Campo("Titolo","Consiste in un nome di fantasia attribuito all'evento",false,STRING);
+	private Campo nPartecipanti=new Campo("Numero di partecipanti"," Stabilisce il numero di persone da coinvolgere nell'evento",true,INT);
 	private Campo tolleranzaPartecipanti=new Campo("Numero di partecipanti in esubero"," Stabilisce il numero di persone in esubero al numero di partecipanti",false,INT);
-	private Campo termineIscrizione=new Campo("Termine ultimo iscrizione","Indica l'ultimo giorno utile per iscriversi all�evento",true,DATE);
-	private Campo luogo=new Campo("Luogo","Indica l'indirizzo in cui si svolger� l�evento oppure il punto di ritrovo",true,STRING);
-	private Campo data=new Campo("Data Evento"," Indica la data in cui si svolger� l'evento, o la data di inizio nel caso l'evento duri pi� giorni",true,DATE);
+	private Campo termineIscrizione=new Campo("Termine ultimo iscrizione","Indica l'ultimo giorno utile per iscriversi all'evento",true,DATE);
+	private Campo luogo=new Campo("Luogo","Indica l'indirizzo in cui si svolgerà l'evento oppure il punto di ritrovo",true,STRING);
+	private Campo data=new Campo("Data Evento"," Indica la data in cui si svolgerà l'evento, o la data di inizio nel caso l'evento duri più giorni",true,DATE);
 	private Campo ora=new Campo("Ora","Indica l'ora in cui i partecipanti dovranno recarisi nel luogo prestabilito",true,ORA);
-	private Campo durata=new Campo("Durata","Indica la durata in termini di numero(approssimativo)di ore e minuti, per gli eventi che si esauriscono in un sol giorno, o in termini di numero esatto di giorni, per gli eventi che occupano pi� giorni consecutivi",false,ORA);
-	private Campo quotaIndividuale=new Campo("Quota individuale"," indica la spesa (o una stima della stessa) che ogni partecipante all�iniziativa dovr� sostenere (si noti che la spesa pu� anche essere nulla)",true,INT);
-	private Campo compresoQuota=new Campo("Compreso nella quota"," indica tutte le voci di spesa comprese nell�ammontare indicato nella �Quota individuale�",false,STRING);
-	private Campo dataFine=new Campo("Data conclusiva"," Indica la data di fine dell�evento",false,DATE);
+	private Campo durata=new Campo("Durata","Indica la durata in termini di numero(approssimativo)di ore e minuti, per gli eventi che si esauriscono in un sol giorno, o in termini di numero esatto di giorni, per gli eventi che occupano più giorni consecutivi",false,ORA);
+	private Campo quotaIndividuale=new Campo("Quota individuale"," indica la spesa (o una stima della stessa) che ogni partecipante all'iniziativa dovrà sostenere (si noti che la spesa può anche essere nulla)",true,INT);
+	private Campo compresoQuota=new Campo("Compreso nella quota"," indica tutte le voci di spesa comprese nell'ammontare indicato nella 'Quota individuale'",false,STRING);
+	private Campo dataFine=new Campo("Data conclusiva"," Indica la data di fine dell'evento",false,DATE);
     private Campo dataRitiroIscrizione=new Campo("Data ritiro iscrizione","Indica la data entro cui sia possibile: per il creatore dell'evento eliminare l'evento, per i partecipanti di annullare l'iscrizione dall'evento",false,DATE);
-    private Campo oraFine=new Campo("Ora conclusiva"," Indica l'ora di fine dell�evento",false,ORA);
+    private Campo oraFine=new Campo("Ora conclusiva"," Indica l'ora di fine dell'evento",false,ORA);
 	private Campo note=new Campo("Note"," Contiene informazioni aggiuntive circa l'evento",false,STRING);
 	private ArrayList<Campo> elencoCampi = new ArrayList<>();
 	
 	
-	//Costruttori
+	/*Costruttori*/
+	/**
+	 * Una categoria e' costituita da nome, descrizione, titolo, numero partecipanti, numero di tolleranza partecipanti, data termine iscrizione,
+	 * luogo, data, ora, durata, quota individuale di partecipazione, compreso nella quota, data finale, data ritiro iscrizione, ora finale,
+	 * note, elenco dei campi.
+	 *
+	 * @param _nome nome della categoria
+	 * @param _descrizione descrizione della categoria
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Categoria(String _nome, String _descrizione){
 		nome=_nome;
 		descrizione= _descrizione;
@@ -60,130 +94,196 @@ public class Categoria implements Serializable{
 		elencoCampi.add(oraFine);
 		elencoCampi.add(note);
 	}
-	
-	//Metodo che permette di inserire i valori ai campi comuni a tutte le categorie
-	public void inserisciCampi()throws Exception{
-		for (int i=0; i< elencoCampi.size(); i++){
-			elencoCampi.get(i).inserisciValore();
-			
-		}
-	}
-	
-	 public void inserisciValoriPredefiniti(){
-	    	
-	    }
-	
-	// Visualizza tutti i campi di una categoria
-	public void visualizzaCampi(){
-		
-		for (int i=0; i< elencoCampi.size(); i++){
-			
-			System.out.println(elencoCampi.get(i).visualizzaCampo());
-		}
-		
-	}
-	
-	public int getPartecipantiMax() {
-		if(tolleranzaPartecipanti.getValore().getInserito()){
-			return (int) nPartecipanti.getValore().getValore() + (int) tolleranzaPartecipanti.getValore().getValore();
-		}
-		else{
-			return (int) nPartecipanti.getValore().getValore();
-		}
-		
-	}
-	
-	public int getPartecipantiMin() {
-		if(tolleranzaPartecipanti.getValore().getInserito()){
-			return (int) nPartecipanti.getValore().getValore() - (int) tolleranzaPartecipanti.getValore().getValore();
-		}
-		else{
-			return (int) nPartecipanti.getValore().getValore();
-		}
-		
-	}
-	
-	// Getters and Setters generati automaticamente
+
+	/*Getters*/
+	/**
+	 * Ritorna il nome della Categoria
+	 * @return nome della categoria
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public String getNome() {
 		return nome;
 	}
-
-
+	/**
+	 * Ritorna la descrizione della Categoria
+	 * @return descrizione del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public String getDescrizione() {
 		return descrizione;
 	}
-
-
+	/**
+	 * Ritorna il titolo dell'evento
+	 * @return titolo dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getTitolo() {
 		return titolo;
 	}
-
-
+	/**
+	 * Ritorna il numero dei partecipanti
+	 * @return numero dei partecipanti
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getnPartecipanti() {
 		return nPartecipanti;
 	}
-
-
+	/**
+	 * Ritorna la data di termine iscrizione
+	 * @return la data di termine iscrizione
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getTermineIscrizione() {
 		return termineIscrizione;
 	}
-
-
+	/**
+	 * Ritorna il luogo dell'evento
+	 * @return luogo dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getLuogo() {
 		return luogo;
 	}
-
-
+	/**
+	 * Ritorna l'ora dell'evento
+	 * @return ora dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getOra() {
 		return ora;
 	}
-
-
+	/**
+	 * Ritorna la durata dell'evento
+	 * @return durata dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getDurata() {
 		return durata;
 	}
-
-
+	/**
+	 * Ritorna la quota individuale di partecipazione
+	 * @return quota individuale
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getQuotaIndividuale() {
 		return quotaIndividuale;
 	}
-
-
+	/**
+	 * Ritorna ciò che è compreso nella quota individuale
+	 * @return compreso nella quota individuale
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getCompresoQuota() {
 		return compresoQuota;
 	}
-
-
+	/**
+	 * Ritorna la data di fine dell'evento
+	 * @return data finale evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getDataFine() {
 		return dataFine;
 	}
-
-
+	/**
+	 * Ritorna l'ora di fine dell'evento
+	 * @return ora finale evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getOraFine() {
 		return oraFine;
 	}
-
-
+	/**
+	 * Ritorna le note dell'evento
+	 * @return note aggiuntive circa l'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public Campo getNote() {
 		return note;
 	}
+	/**
+	 * Ritorna un ArrayList dei campi dell'evento
+	 * @return ArrayList dei campi dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public ArrayList<Campo> getElencoCampi() {
+		return elencoCampi;
+	}
+	/**
+	 * Ritorna la data dell'evento
+	 * @return data dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public Campo getData() {
+		return data;
+	}
+	/**
+	 * Ritorna il numero di partecipanti in esubero
+	 * @return tolleranza numero partecipanti
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public Campo getTolleranzaPartecipanti() {
+		return tolleranzaPartecipanti;
+	}
+	/**
+	 * Ritorna la data di ritiro iscrizione e di eliminazione dell'evento
+	 * @return data di ritiro iscrizione e di eliminazione dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
+	public Campo getDataRitiroIscrizione() {
+		return dataRitiroIscrizione;
+	}
 
-
+	/*Setters*/
+	/**
+	 * Metodo che imposta il nome della Categoria
+	 * @return nome della categoria
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-
+	/**
+	 * Metodo che imposta la descrizione della Categoria
+	 * @return descrizione del campo
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
-
-
+	/**
+	 * Metodo che imposta il titolo dell'evento
+	 * @return titolo dell'evento
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public void setTitolo(Campo titolo) {
 		this.titolo = titolo;
 	}
-
-
+	/**
+	 * Metodo che imposta il numero di partecipanti
+	 * @return numero partcipanti
+	 *
+	 * @author Gabriele Manenti
+	 */
 	public void setnPartecipanti(Campo nPartecipanti) {
 		this.nPartecipanti = nPartecipanti;
 	}
@@ -233,35 +333,66 @@ public class Categoria implements Serializable{
 		this.note = note;
 	}
 
-	public ArrayList<Campo> getElencoCampi() {
-		return elencoCampi;
-	}
+
 
 	public void setElencoCampi(ArrayList<Campo> elencoCampi) {
 		this.elencoCampi = elencoCampi;
-	}
-
-	public Campo getData() {
-		return data;
 	}
 
 	public void setData(Campo data) {
 		this.data = data;
 	}
 
-    public Campo getTolleranzaPartecipanti() {
-        return tolleranzaPartecipanti;
-    }
 
-    public void setTolleranzaPartecipanti(Campo tolleranzaPartecipanti) {
-        this.tolleranzaPartecipanti = tolleranzaPartecipanti;
-    }
+	public void setTolleranzaPartecipanti(Campo tolleranzaPartecipanti) {
+		this.tolleranzaPartecipanti = tolleranzaPartecipanti;
+	}
 
-    public Campo getDataRitiroIscrizione() {
-        return dataRitiroIscrizione;
-    }
+	public void setDataRitiroIscrizione(Campo dataRitiroIscrizione) {
+		this.dataRitiroIscrizione = dataRitiroIscrizione;
+	}
 
-    public void setDataRitiroIscrizione(Campo dataRitiroIscrizione) {
-        this.dataRitiroIscrizione = dataRitiroIscrizione;
-    }
+	//Metodo che permette di inserire i valori ai campi comuni a tutte le categorie
+	public void inserisciCampi()throws Exception{
+		for (int i=0; i< elencoCampi.size(); i++){
+			elencoCampi.get(i).inserisciValore();
+			
+		}
+	}
+	
+	 public void inserisciValoriPredefiniti(){
+	    	
+	    }
+	
+	// Visualizza tutti i campi di una categoria
+	public void visualizzaCampi(){
+		
+		for (int i=0; i< elencoCampi.size(); i++){
+			
+			System.out.println(elencoCampi.get(i).visualizzaCampo());
+		}
+		
+	}
+	
+	public int getPartecipantiMax() {
+		if(tolleranzaPartecipanti.getValore().getInserito()){
+			return (int) nPartecipanti.getValore().getValore() + (int) tolleranzaPartecipanti.getValore().getValore();
+		}
+		else{
+			return (int) nPartecipanti.getValore().getValore();
+		}
+		
+	}
+	
+	public int getPartecipantiMin() {
+		if(tolleranzaPartecipanti.getValore().getInserito()){
+			return (int) nPartecipanti.getValore().getValore() - (int) tolleranzaPartecipanti.getValore().getValore();
+		}
+		else{
+			return (int) nPartecipanti.getValore().getValore();
+		}
+		
+	}
+	
+
 }
